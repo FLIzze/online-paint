dda12981caf926dc3eef0797d2559aff3ec22a95
flizze <alexandre.bel@hotmail.com>
2024-05-16 13:32:50 +0200

    made div focusable

--- components/draggable/page.tsx
+++ components/draggable/page.tsx
@@ -49,6 +49,7 @@ export default function Draggable({ children, name, posX, posY }: Readonly<Dragg
             onMouseDown={handleMouseDown}
             className='absolute cursor-move'
             role='button'
+            tabIndex={0}
         >
             <div className='border border-black pb-2 bg-[#494949]'>
                 <p className='bg-[#494949] text-sm p-1 mb-2 text-white'>{name}</p>
