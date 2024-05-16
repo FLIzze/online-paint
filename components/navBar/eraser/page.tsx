03c7b29c16d3bf474c6d6cebfd79b4398122ca16
flizze <alexandre.bel@hotmail.com>
2024-05-16 12:53:45 +0200

    fixing errors

--- components/navBar/eraser/page.tsx
+++ components/navBar/eraser/page.tsx
@@ -2,35 +2,37 @@ import setEraser from "@/app/ts/utils/setEraser";
 import Brush from "@/app/ts/class/brush";
 import Image from "next/image";
 
-export default function Eraser({ draw, setDraw, usedTool, setUsedTool }: { draw: Brush, setDraw: React.Dispatch<React.SetStateAction<Brush>>, setUsedTool: React.Dispatch<React.SetStateAction<boolean>>, usedTool: boolean}) {
+export default function Eraser({ draw, setDraw, usedTool, setUsedTool }: { draw: Brush, setDraw: React.Dispatch<React.SetStateAction<Brush>>, setUsedTool: React.Dispatch<React.SetStateAction<boolean>>, usedTool: boolean }) {
     return (
         <div>
             {usedTool == true ? (
-                <button>
+                <button
+                    onClick={() => {
+                        setUsedTool(false);
+                        setEraser(draw, setDraw);
+                    }}
+                >
                     <Image
-                        src="eraser.png"
+                        src="/eraser.png"
                         alt="eraser"
-                        className="p-1 rounded-sm bg-[#647c91] border border-black"
-                        onClick={() => {
-                            setUsedTool(false);
-                            setEraser(draw, setDraw);
-                        }}
-                        width={32}
-                        height={32}
+                        className="m-1 rounded-sm bg-[#647c91] border border-black"
+                        width={24}
+                        height={24}
                     />
                 </button>
             ) : (
-                <button>
+                <button
+                    onClick={() => {
+                        setUsedTool(true);
+                        setEraser(draw, setDraw);
+                    }}
+                >
                     <Image
-                        src="eraser.png"
+                        src="/eraser.png"
                         alt="eraser"
-                        className="p-1 rounded-sm border border-transparent hover:border-black"
-                        onClick={() => {
-                            setUsedTool(true);
-                            setEraser(draw, setDraw);
-                        }}
-                        width={32}
-                        height={32}
+                        className="m-1 rounded-sm border border-transparent hover:border-black"
+                        width={24}
+                        height={24}
                     />
                 </button>
             )}
--- components/utils/basicsTool/page.tsx
+++ components/utils/basicsTool/page.tsx
@@ -1,21 +1,16 @@
 import Brush from "@/app/ts/class/brush";
 import setTool from "@/app/ts/utils/setTool";
-import Image from "next/image";
 
 export default function BasicTools({ setDraw, setUsedTool, usedTool, tool }: { setDraw: React.Dispatch<React.SetStateAction<Brush>>, setUsedTool: React.Dispatch<React.SetStateAction<string>>, usedTool: string, tool: string }) {
     return (
         <div>
             {usedTool == tool ? (
-                <div>
-                    <Image
-                        src={`/${tool}.png`}
-                        alt={tool}
-                        className="p-1 rounded-sm bg-[#647c91] border border-black"
-                        title={tool}
-                        width={32}
-                        height={32}
-                    />
-                </div>
+                <img
+                    src={`/${tool}.png`}
+                    alt={tool}
+                    className="h-8 w-8 p-1 rounded-sm bg-[#647c91] border border-black"
+                    title={tool}
+                />
             ) : (
                 <button
                     onClick={() => {
@@ -23,13 +18,11 @@ export default function BasicTools({ setDraw, setUsedTool, usedTool, tool }: { s
                         setUsedTool(tool);
                     }}
                 >
-                    <Image
+                    <img
                         src={`/${tool}.png`}
                         alt={tool}
-                        className="p-1 rounded-sm border border-transparent hover:border-black"
+                        className="h-8 w-8 p-1 rounded-sm border border-transparent hover:border-black"
                         title={tool}
-                        width={32}
-                        height={32}
                     />
                 </button>
             )}
